2b2a49fa70d4afb89a9eb6499fe53a8cc8c7c93a
cr0manty <36458874+cr0manty@users.noreply.github.com>
2020-02-15 16:12:33 +0200

    add migrations

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 
-from .models import Log
+from .models import Log, RQLog
 
 
+@admin.register(Log)
 class LogAdmin(admin.ModelAdmin):
     pass
 
 
-admin.site.register(Log, LogAdmin)
+@admin.register(RQLog)
+class RQLogAdmin(admin.ModelAdmin):
+    pass
