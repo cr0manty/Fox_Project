37bc0ad56ccc788fa407c33b54013a85d3fcb2b8
cr0manty <36458874+cr0manty@users.noreply.github.com>
2020-04-03 00:35:51 +0300

    fixes

--- youtube_saver/admin.py
+++ youtube_saver/admin.py
@@ -5,7 +5,11 @@ from youtube_saver.models import YoutubePosts, YoutubeFormats
 
 @admin.register(YoutubePosts)
 class YoutubePostsAdmin(admin.ModelAdmin):
-    pass
+    def has_add_permission(self, request):
+        return False
 
 
-admin.site.register(YoutubeFormats)
+@admin.register(YoutubeFormats)
+class YoutubeFormatsAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False
