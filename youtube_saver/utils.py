84947868366ddd02df60f7a391a7c5cfe1d0b0d4
cr0manty <aappland@gmail.com>
2020-05-16 16:44:45 +0300

    update utils

--- youtube_saver/utils.py
+++ youtube_saver/utils.py
@@ -2,10 +2,12 @@ import uuid
 import io
 import os
 import requests
+
+from datetime import datetime, timedelta
+
 from django.conf import settings
-from django_rq import job
+from django_rq import get_scheduler
 from pydub import AudioSegment
-from rq.dummy import sleep
 
 from core.models import TelegramBotLogs
 
@@ -27,22 +29,21 @@ def send_telegram_audio(bot, message, url, title, duration):
             print('Sending File')
             bot.send_chat_action(message.chat.id, action='upload_document')
             bot.send_audio(message.chat.id, audio=audio, title=title, duration=duration)
-            delete_file.delay(filename)
+            start_delete(filename)
         else:
             print('Not Found')
-            TelegramBotLogs.objects.create(**TelegramBotLogs.get_kwargs(message, e=url,  log_type=2))
-
+            start_delete(filename)
+            TelegramBotLogs.objects.create(**TelegramBotLogs.get_kwargs(message, e=url, log_type=2))
     except Exception as e:
         print(e)
-        delete_file.delay(filename)
         TelegramBotLogs.objects.create(**TelegramBotLogs.get_kwargs(message, e=e, log_type=1))
 
 
-@job
+def start_delete(filename):
+    scheduler = get_scheduler('default')
+    scheduler.enqueue_at(datetime.now() + timedelta(minutes=20), delete_file, filename=filename)
+
+
 def delete_file(filename):
     if os.path.exists(filename):
-        try:
-            os.remove(filename)
-        except OSError:
-            sleep(200)
-            delete_file.delay(filename)
+        os.remove(filename)
