80ea171e044542feb370d38c7dc825acc8fada0f
cr0manty <aappland@gmail.com>
2020-07-01 02:15:44 +0300

    fix vk music update

--- songs/api/views.py
+++ songs/api/views.py
@@ -1,3 +1,7 @@
+import datetime
+import re
+import json
+
 from django.utils import timezone
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -7,7 +11,7 @@ from rest_framework.authentication import TokenAuthentication
 
 from django.utils.timezone import now
 from django.db.models import Q
-from vk_audio import audio
+from vk_audio import audio, Audio_obj
 
 from api.serializers import StandardResultsSetPagination
 from core.views import AmountModelViewSet
@@ -78,6 +82,10 @@ class AddSongFromUser(APIView):
 
 
 class UserSongListAPIView(AmountModelViewSet, VKAuthMixin):
+    vk_session = None
+    uid = None
+    ans_stupid = None
+
     permission_classes = (IsAuthenticated,)
     authentication_classes = (TokenAuthentication,)
     queryset = Song.objects.all().order_by('song_id')
@@ -87,52 +95,93 @@ class UserSongListAPIView(AmountModelViewSet, VKAuthMixin):
     def get_filter_query(self):
         song_id = self.request.GET.get('song_id', None)
         yesterday = timezone.now() - timezone.timedelta(days=1)
-        query = Q(users=self.request.user) & Q(created_at__gte=yesterday)
+        query = Q(users=self.request.user) & Q(updated_at__gte=yesterday)
 
         if song_id is not None:
             return query & Q(song_id=song_id)
         return query
 
+    def _as_object(self, i):
+        q = self.ans_stupid[i]
+        id = i.split("_")[1]
+        owner_id = i.split("_")[0]
+        sig = q[1][13].split("/")
+        sig = sig[2] + "_" + sig[5]
+        return Audio_obj(id, owner_id, sig,
+                         q[1][3], q[1][4], q[1][5],
+                         q[1][14].split(",")[0],
+                         self.encode_url, self.vk_session
+                         )
+
+    def encode_url(this, url):
+        from vk_audio.decoder import decode_audio_url
+        return decode_audio_url(url, this.uid)
+
+    def _set_headers(self):
+        self.vk_session.http.headers['Upgrade-Insecure-Requests'] = "1"
+        self.vk_session.http.cookies.set('remixaudio_background_play_time_', '0')
+        self.vk_session.http.cookies.set('remixaudio_background_play_time_limit', '1800')
+        self.vk_session.http.cookies.set('remixaudio_show_alert_today', '0')
+        self.vk_session.http.cookies.set('remixff', '10')
+        self.vk_session.http.cookies.set('remixmaudioq', '')
+        self.vk_session.http.cookies.set('remixaudio_date', datetime.datetime.now().date().strftime("%Y-%m-%d"))
+        self.vk_session.http.cookies.set('remixmdevice', '1280/800/1/!!-!!!!')
+        self.vk_session.http.headers['X-Requested-With'] = 'XMLHttpRequest'
+
+    def _get_vk_songs(self):
+        self._set_headers()
+        del self.vk_session.http.headers['X-Requested-With']
+        text = self.vk_session.http.get(
+            f"https://m.vk.com/audio{('s' + str(self.uid)) if self.uid is not None else ''}").text
+        self.vk_session.http.headers['X-Requested-With'] = 'XMLHttpRequest'
+        all_audio = re.findall(
+            '"_cache":(.+?),"soft_filter":true|false,"need_invalid_keys":true|false,"top_len":\d+,', text)
+        try:
+            all_audio = all_audio[0][:all_audio[0].rfind(',')]
+            self.ans_stupid = json.loads(all_audio)
+        except:
+            self.ans_stupid = json.loads(all_audio[0] if len(all_audio) > 0 and len(all_audio[0]) != 0 else "{}")
+        return list(map(self._as_object, self.ans_stupid))
+
     def create(self, request, *args, **kwargs):
         user = request.user
-        if user.last_songs_update <= timezone.now() - timezone.timedelta(hours=3):
+        try:
+            self.vk_session = self.try_auth(request.POST, user.vk_login, token=user.auth_token)
+            self.uid = self.vk_session.method("users.get")[0]["id"]
+            if self.captcha_url:
+                return Response({'url': self.captcha_url, 'sid': self.captcha_sid}, status=302)
+
+            audio_list = self._get_vk_songs()
+        except Exception as e:
+            err_text = 'Cant connect to vk server'
+            Log.objects.create(exception=str(e), additional_text=err_text, from_user=user.username)
+            return Response(err_text, status=400)
+
+        for track in audio_list:
             try:
-                vk_session = self.try_auth(request.POST, user.vk_login, token=user.auth_token)
-                if self.captcha_url:
-                    return Response({'url': self.captcha_url, 'sid': self.captcha_sid}, status=302)
-
-                audio_obj = audio(vk_session)
-                audio_list = audio_obj.get()
-            except Exception as e:
-                err_text = 'Cant connect to vk server'
-                Log.objects.create(exception=str(e), additional_text=err_text, from_user=user.username)
-                return Response(err_text, status=400)
-
-            for track in audio_list:
                 try:
-                    try:
-                        song = Song.objects.get(song_id=track.id)
-                        if user not in song.users_ignore.all():
-                            song.users.add(user)
-                        song.download = track.url
-                        song.updated_at = now()
-                        song.save()
-                    except Song.DoesNotExist:
-                        song = Song.objects.create(
-                            song_id=track.id,
-                            artist=track.artist,
-                            title=track.title,
-                            image=track.image,
-                            full_id=track.full_id,
-                            duration=track.duration,
-                            download=track.url,
-                        )
-                        song.save()
+                    song = Song.objects.get(song_id=track.id)
+                    if user not in song.users_ignore.all():
                         song.users.add(user)
-                except Exception as e:
-                    Log.objects.create(exception=str(e), from_user=user.username)
-            user.last_songs_update = timezone.now()
-            user.save()
+                    song.download = track.url
+                    song.updated_at = now()
+                    song.save()
+                except Song.DoesNotExist:
+                    song = Song.objects.create(
+                        song_id=track.id,
+                        artist=track.artist,
+                        title=track.title,
+                        image=track.image,
+                        full_id=track.full_id,
+                        duration=track.duration,
+                        download=track.url,
+                    )
+                    song.save()
+                    song.users.add(user)
+            except Exception as e:
+                Log.objects.create(exception=str(e), from_user=user.username)
+        user.last_songs_update = timezone.now()
+        user.save()
         return self.list(request, *args, **kwargs)
 
 
